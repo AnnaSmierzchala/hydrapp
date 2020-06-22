b5c7f8d98a76d3a3f103c3fc7664d43f225fe59d
Anna Śmierzchała <smierzchala.anna@gmail.com>
2020-06-22 23:45:13 +0200

    add glasses counting for button 'add glass'

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,15 @@ registerSW();
 
 console.log('HELLO 🚀')
 
+
+
+let counter = document.querySelector('.counter--js');
+//let counterValue = counter.value
+//counter.value = counterValue + 1 
+
+let myNumber = 0 
+
+
 const btnAddGlass = document.querySelector('.glass-add--js')
 const water = document.querySelector('.animate')
 
@@ -16,6 +25,8 @@ e.preventDefault
 water.classList.remove('animate-add--js')
 void btnAddGlass.offsetWidth;
 water.classList.add('animate-add--js')
+counter.innerHTML = myNumber + 1 
+myNumber = myNumber + 1;
 
 });
 
