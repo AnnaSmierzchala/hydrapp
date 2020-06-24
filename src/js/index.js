0812021b79dd4f15376ac9ed3d6543c45b0791b4
Anna Śmierzchała <smierzchala.anna@gmail.com>
2020-06-24 21:48:47 +0200

    add local storage and buttons events

--- src/js/index.js
+++ src/js/index.js
@@ -10,23 +10,71 @@ console.log('HELLO 🚀')
 
 
 
-let counter = document.querySelector('.counter--js');
+
 //let counterValue = counter.value
 //counter.value = counterValue + 1 
 
-let myNumber = 0 
 
 
+let counter = document.querySelector('.counter--js');
 const btnAddGlass = document.querySelector('.glass-add--js')
+const btnRemoveGlass = document.querySelector('.glass-remove--js')
 const water = document.querySelector('.animate')
 
-btnAddGlass.addEventListener('click', function(e) {
-e.preventDefault
+
+const key = new Date() .toISOString() .slice(0, 10)
+
+let glassesAmount = localStorage.getItem(key);
+//let result = ''
+
+if (glassesAmount) {
+    console.log(`ilość szklanek to ${glassesAmount}`)
+//result = glassesAmount
+
+} else {
+ glassesAmount = 0
+ localStorage.setItem(key, glassesAmount)
+}
+
+counter.innerHTML = glassesAmount
+
+
+btnAddGlass.addEventListener('click', function() {
+water.classList.remove('animate-remove--js')
 water.classList.remove('animate-add--js')
 void btnAddGlass.offsetWidth;
 water.classList.add('animate-add--js')
-counter.innerHTML = myNumber + 1 
-myNumber = myNumber + 1;
+
+++glassesAmount
+counter.innerHTML = glassesAmount
+//myNumber = myNumber + 1;
+
+localStorage.setItem(key, counter.innerHTML);
+//counter.innerHTML = glassesAmount + 1
+
 
 });
+
+
+btnRemoveGlass.addEventListener('click', function() {
+    if (glassesAmount > 0) {
+        water.classList.remove('animate-add--js')
+    water.classList.remove('animate-remove--js')
+    void btnRemoveGlass.offsetWidth;
+    water.classList.add('animate-remove--js')
+    counter.innerHTML = --glassesAmount
+//myNumber = myNumber - 1;
+    }
+
+   
+localStorage.setItem(key, counter.innerHTML);
+//counter.innerHTML = glassesAmount
+
+
+})
+
+
+
+
+
 
